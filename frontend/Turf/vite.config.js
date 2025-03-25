672c4f9ccca022c9d631d79bf4ae5b101e38e97d
unknown <dineshkarthick@110gmail.com>
2025-03-25 01:10:43 -0700

    updated

--- frontend/Turf/vite.config.js
+++ frontend/Turf/vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'frontend/Turf/dist', 
-  }
+  plugins: [react()]
 })
