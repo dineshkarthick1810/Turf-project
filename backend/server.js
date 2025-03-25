f5b280d5886256f3eb2f4f89616e67eec9e9f4d6
unknown <dineshkarthick@110gmail.com>
2025-03-24 23:04:22 -0700

    vercel added

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,13 @@ const express=require("express")
 const cors=require("cors")
 const routes=require("./routes/route")
 const app=express()
-// const bodyparser=require("body-parser")
+const bodyparser=require("body-parser")
 
 app.use(cors())
 app.use(express.json())
 app.use(express.static("./uploads"))
 
-// app.use(bodyparser.urlencoded())
+app.use(bodyparser.urlencoded())
 
 app.use("/api",routes)
 
@@ -18,11 +18,14 @@ app.use("/api",routes)
 
 
 
+app.listen(3002,()=>{
+    console.log("server started")
+
+}
 
 
 
-
-module.exports=app
+)
 
 
 
