20924b57d027ad0dfa0bac21873067b96ed12d23
unknown <dineshkarthick@110gmail.com>
2025-03-25 02:36:09 -0700

    cors header updated

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,12 @@ const app=express()
 const bodyparser=require("body-parser")
 
 const corsOptions = {
-    origin: 'https://turf-project-gold.vercel.app', 
+    origin: 'https://turf-project-gold.vercel.app',  // Frontend URL
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,  // If you need to include cookies or credentials in the request
+    preflightContinue: false,  // This is important for preflight requests
+    optionsSuccessStatus: 204,  // For legacy browsers
   };
 
 app.use(cors(corsOptions))
@@ -15,6 +18,8 @@ app.use(express.json())
 app.use(express.static("./uploads"))
 
 app.use(bodyparser.urlencoded())
+
+app.options("*", cors(corsOptions));
 
 app.use("/api",routes)
 
