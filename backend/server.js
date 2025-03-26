addaeca074d6bbe0088bb804f3e011547c453907
unknown <dineshkarthick@110gmail.com>
2025-03-25 20:59:17 -0700

    changes

--- backend/server.js
+++ backend/server.js
@@ -4,18 +4,14 @@ const routes=require("./routes/route")
 const app=express()
 const bodyparser=require("body-parser")
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://turf-project-gold.vercel.app'); // Your frontend URL
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+const corsOptions = {
+    origin: 'https://turf-project-gold.vercel.app',  
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+  };
 
-  // Handle pre-flight request
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();  // Respond to OPTIONS requests
-  }
-
-  next();  // Proceed to the next middleware/route
-});
+app.options("*",cors(corsOptions))
+app.use(cors(corsOptions))
 
 app.use(express.json())
 app.use(express.static("./uploads"))
