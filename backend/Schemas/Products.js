14eaf19d872cc51ca333c07233806e5ecfcc709f
unknown <dineshkarthick@110gmail.com>
2025-03-27 02:02:46 -0700

    changes

--- backend/Schemas/Products.js
+++ backend/Schemas/Products.js
@@ -1,5 +1,10 @@
 const mongoose=require("mongoose")
-mongoose.connect("mongodb+srv://dineshkarthick1810:dinesh18@cluster0.1bvkbxq.mongodb.net/Adminusers")
+mongoose.connect("mongodb+srv://dineshkarthick1810:dinesh18@cluster0.1bvkbxq.mongodb.net/Adminusers", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000, // Increase timeout
+    socketTimeoutMS: 45000, // Increase socket timeout
+  })
 
 const Schema=new mongoose.Schema({
     image:String,
