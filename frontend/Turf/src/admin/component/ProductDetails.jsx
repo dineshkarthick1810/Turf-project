6eb7c148a010127a22c200f5901cf4b876732a8f
unknown <dineshkarthick@110gmail.com>
2025-03-28 23:51:22 -0700

    table responsive added

--- frontend/Turf/src/admin/component/ProductDetails.jsx
+++ frontend/Turf/src/admin/component/ProductDetails.jsx
@@ -28,7 +28,7 @@ const ProductDetails = ({Phonenumber}) => {
    return <div style={{overflowX:"scroll"}}>
 <img src={`https://turf-project-04az.onrender.com/images/${val.image}`} style={{width:"100%",height:"80vh"}} className='img-fluid'/>
 <p className='text-muted mt-3'>{val.description}</p>
-<table className='table table-dark mt-5' >
+<table className='table table-dark mt-5 fortable' >
 
     <thead>
 <tr>
